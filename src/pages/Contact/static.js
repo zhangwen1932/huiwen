97efe460af71251939f4fb727ba36e1fc43e4e05
ZhangWen <zhangwen1932@hotmail.com>
2023-01-21 01:30:59 +0800

    添加静态数据

---
+++ src/pages/Contact/static.js
@@ -0,0 +1,18 @@
+export const bannerText = {
+  title: '联系我们',
+  desc: ['致力于为客户的家族实现代际传承、家族兴旺、基业长青。',
+  '构筑保护壁垒应对全生命周期的诸多挑战'],
+}
+
+export const contactText = {
+  title: '我们的联系方式-汇文咨询',
+  content: {
+    phone: '+61 401308770',
+    email: 'info@wyvernadvisory.com', 
+    address: 'No. 13 Rawson Street, Rockdale NSW 2216',
+  }
+}
+
+export const correlationText = {
+  title: '关联持牌机构',
+}

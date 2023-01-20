e11059ff13ce0b8fd083b52c6be6243bbbf78164
ZhangWen <zhangwen1932@hotmail.com>
2023-01-20 11:36:19 +0800

    调整banner

--- src/pages/About/components/Banner/index.jsx
+++ src/pages/About/components/Banner/index.jsx
@@ -3,6 +3,7 @@ import './styles.scss';
 
 const Banner = () => (
   <div className="banner section-item">
+    <div className="mask" />
     <div className="section-content">
       <div className="middle-value">
         <div className="banner-title">
--- src/pages/About/components/CoreCompetence/index.jsx
+++ src/pages/About/components/CoreCompetence/index.jsx
@@ -3,7 +3,7 @@ import { coreCompetenceText } from '../../static';
 console.log('coreCompetenctText', coreCompetenceText);
 
 const CoreCompetence = () => (
-  <div className="core-competence section-ele">
+  <div className="core-competence section-item">
     <div className="section-content">
       <div className="middle-value">
         <div className="title">

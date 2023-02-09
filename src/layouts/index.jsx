6eebac05d49ac78ecb21769015fdc0b2e5bd8642
sorwnqe@outlook.com <534534123yu>
2023-02-09 18:42:30 +0800

    fix: anchor切换路由引发的scroll问题

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import { Outlet } from 'umi';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'umi';
 import { Layout } from 'antd';
 import HeaderComponent from './Header';
-import FooterComponent from './Footer'
- 
+import FooterComponent from './Footer';
+
 import 'animate.css';
 import './styles.scss';
 
 const { Header, Content, Footer } = Layout;
 
-const LayoutComponent = () => (
-  <>
-    <Header style={{ position: 'fixed', top: 0, zIndex: 1, width: '100%', height: '50px', backgroundColor: '#fff' }}>
-      <HeaderComponent />
-    </Header>
-    <Content className='content'>
-      <Outlet />
-    </Content>
-    <Footer >
-      <FooterComponent />
-    </Footer>
-  </>
-);
+const LayoutComponent = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return (
+    <>
+      <Header
+        style={{
+          position: 'fixed',
+          top: 0,
+          zIndex: 1,
+          width: '100%',
+          height: '50px',
+          backgroundColor: '#fff',
+        }}
+      >
+        <HeaderComponent />
+      </Header>
+      <Content className="content">
+        <Outlet />
+      </Content>
+      <Footer>
+        <FooterComponent />
+      </Footer>
+    </>
+  );
+};
 
 export default LayoutComponent;
